78e9c3436c72eecaa189a66d5ef611cd8badf5b7
Alexander Schiller <alexschiller@gmail.com>
2014-03-06 09:59:26 -0500

    Remove local testing option

--- R/osf_get_projects.r
+++ R/osf_get_projects.r
@@ -3,16 +3,10 @@
 #' @import RCurl rjson
 #' @export
 
-osf_get_projects <- function(user_url, local=FALSE){
-    url <- gsub("user_url", user_url, "https://osf.io/api/v1/profile/user_url/public_projects/")
+osf_get_projects <- function(user_url){
+    url <- gsub("user_url", user_url, "https://osf.io/api/v1/profile/user_url/public_projects/")}
     
-    ##### For testing purposes ##### RM for production
-    if(local==TRUE){
-        url <- gsub("user_url", user_url, "localhost:5000/api/v1/profile/user_url/public_projects/")
-    }
-    
-    json_data <- getURL(url)
-    json_data <- fromJSON(json_data, method = "C", unexpected.escape = "error" )
+    json_data <- fromJSON(getURL(url), method = "C", unexpected.escape = "error" )
     
     df <- data.frame("projects"=character())  #todo need to add name to OSF route, easy
     
